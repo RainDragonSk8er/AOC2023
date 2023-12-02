732064ab733423b22f8d5ce39cc402078ce2be98
Jonathan Elias Holme <whalerus.iceberg@fjellstua.net>
2023-12-02 17:56:43 +0100

    made a nicer looking file structure

--- bin/U-Utilities/treemaker.sh
+++ bin/U-Utilities/treemaker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 for i in {1..25..1}; do 
     for name in "jonathan" "theodor" "kristian" "hallvard"; do
-        mkdir -p Des${i}/${name}
-        touch Des${i}/${name}/.hiddenfile
+        printf -v j "%02d" $i
+        path="Des-${j}/${name}"
+        mkdir -p ${path}
+        touch ${path}/.hiddenfile
     done
 done
