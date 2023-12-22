29a3faf49fc27b3824566a24e60fe974add4d662
halholme <hh@revisoft.no>
2023-12-22 16:37:20 +0100

    HH solved 21 and 22

---
+++ Des-21/hallvard/Program.cs
@@ -0,0 +1,194 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Drawing;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+
+public static class Program
+{
+    static HashSet<Plot> rocks = new HashSet<Plot>();
+    static HashSet<Plot>[] steps = new HashSet<Plot>[101];
+
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static int[] startx = [0, 65, 130, 130, 130, 65, 0, 0];
+    static int[] starty = [0, 0, 0, 65, 130, 130, 130, 65];
+    static int[] count64 = [202300, 0, 202300, 0, 202300, 0, 202300, 0];
+    static int[] count130 = [0, 1, 0, 1, 0, 1, 0, 1];
+    static int[] count195 = [202299, 0, 202299, 0, 202299, 0, 202299, 0];
+
+    public static int xlen, ylen;
+
+    static void Main()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        Console.WriteLine("Hello World on December 21th 2023!");
+
+        
+        string inputPath = @"..\..\..\AOC2023-21-Input.txt";
+        using (StreamReader inputFile = new StreamReader(inputPath))
+        {
+            string input;
+            int y = 0;
+            steps[0] = new HashSet<Plot>();
+
+            while ((input = inputFile.ReadLine()) != null)
+            {
+                xlen = Math.Max(xlen, input.Length);
+
+                for (int x = 0; x < input.Length; x++)
+                {
+                    if (input[x] == '#')
+                        rocks.Add(new Plot(x, y));
+                    else if (input[x] == 'S')
+                    {
+                        steps[0].Add(new Plot(x, y));
+                        Console.WriteLine($"Starting point S is at {x}, {y}.");
+                    }
+
+                }
+                y++;
+            }
+            ylen = y;
+            inputFile.Close();
+            Console.WriteLine("Done reading modules!");
+
+            for (int s = 0; s < 64; s++)
+            {
+                steps[s + 1] = new HashSet<Plot>();
+
+                foreach (Plot p in steps[s])
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        Plot nextstep = new Plot(p.x + dx[i], p.y + dy[i]);
+                        if (nextstep.IsValid(rocks, false))
+                            steps[s + 1].Add(nextstep);
+                    }
+                }
+            }
+
+            int answer = steps[64].Count();
+
+            Console.WriteLine($"The answer to part one is {answer} possible plot destinations after 64 steps.");
+            Console.WriteLine($"Box size is {xlen}, {ylen}.");
+            Console.ReadKey();
+
+            // Part 2
+            UInt64 answer2 = 0;
+            for (int j = 0; j < startx.Length; j++)
+            {
+                steps[0].Clear();
+                steps[0].Add(new Plot(startx[j], starty[j]));
+                Console.WriteLine($"Starting at {startx[j]}, {starty[j]}:");
+
+                int maxcount = 0;
+                int countdown = 500;
+                for (int s = 0; s < 300 && countdown > 0; s++)
+                {
+                    steps[(s + 1) % 2].Clear();
+
+                    foreach (Plot p in steps[s % 2])
+                    {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            Plot nextstep = new Plot(p.x + dx[i], p.y + dy[i]);
+                            if (nextstep.IsValid(rocks, false))
+                                steps[(s + 1) % 2].Add(nextstep);
+                        }
+                    }
+
+                    if (s + 1 == 64 && count64[j] > 0)
+                    {
+                        Console.WriteLine($"Type {startx[j]}, {starty[j]} with {s + 1} steps has a count of {steps[(s + 1) % 2].Count()} plots and {count64[j]} occurences contributing to the grand total with: {steps[(s + 1) % 2].Count() * count64[j]}");
+                        answer2 += (UInt64)(steps[(s + 1) % 2].Count() * count64[j]);
+                    }
+                    if (s + 1 == 130 && count130[j] > 0)
+                    {
+                        Console.WriteLine($"Type {startx[j]}, {starty[j]} with {s + 1} steps has a count of {steps[(s + 1) % 2].Count()} plots and {count130[j]} occurences contributing to the grand total with: {steps[(s + 1) % 2].Count() * count130[j]}");
+                        answer2 += (UInt64)(steps[(s + 1) % 2].Count() * count130[j]);
+                    }
+                    if (s + 1 == 195 && count195[j] > 0)
+                    {
+                        Console.WriteLine($"Type {startx[j]}, {starty[j]} with {s + 1} steps has a count of {steps[(s + 1) % 2].Count()} plots and {count195[j]} occurences contributing to the grand total with: {steps[(s + 1) % 2].Count() * count195[j]}");
+                        answer2 += (UInt64)(steps[(s + 1) % 2].Count() * count195[j]);
+                    }
+                    if (maxcount < steps[(s + 1) % 2].Count() || maxcount < 7560)
+                    {
+                        maxcount = steps[(s + 1) % 2].Count();
+                    }
+                    else
+                    {
+                        if (countdown > 4)
+                        {
+                            Console.WriteLine($"Reached maxcount of {maxcount} reachable garden plots after {s} steps.");
+                            countdown = 4;
+                        }
+                    }
+
+                    countdown--;
+                    if (countdown < 4)
+                        Console.WriteLine($"After {s + 1} steps the count of reachable garden plots is {steps[(s + 1) % 2].Count()}.");
+                }
+            }
+            Console.WriteLine($"Count of filled maps in plot state 7568 is {(UInt64)202300 * (UInt64)202300} contributing to the grand total with: {(UInt64)202300 * (UInt64)202300 * (UInt64)7568}.");
+            answer2 += (UInt64)202300 * (UInt64)202300 * (UInt64)7568;
+            Console.WriteLine($"Count of filled maps in plot state 7567 is {(UInt64)202299 * (UInt64)202299} contributing to the grand total with: {(UInt64)202299 * (UInt64)202299 * (UInt64)7567}.");
+            answer2 += (UInt64)202299 * (UInt64)202299 * (UInt64)7567;
+            Console.WriteLine($"The answer to part two is {answer2} possible plot destinations after 26 501 365 steps.");
+
+            Console.WriteLine("Hit any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
+
+class Plot
+{
+    public int x;
+    public int y;
+
+    public Plot(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public bool IsValid(HashSet<Plot> rocks, bool infinite)
+    {
+        if (infinite)
+        {
+            if (rocks.Contains(new Plot(this.x % Program.xlen, this.y % Program.ylen))) // Hit a rock
+                return false;
+        }
+        else
+        {
+            if (x < 0 || x >= Program.xlen || y < 0 || y >= Program.ylen) // Out of bounds
+                return false;
+
+            if (rocks.Contains(this)) // Hit a rock
+            return false;
+        }
+
+        return true;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || !(obj is Plot))
+            return false;
+
+        Plot plot = (Plot)obj;
+        return this.x == plot.x && this.y == plot.y;
+    }
+
+    public override int GetHashCode()
+    {
+        int hash = 17;
+        hash = hash * 23 + x.GetHashCode();
+        hash = hash * 23 + y.GetHashCode();
+        return hash;
+    }
+}
---
+++ Des-22/hallvard/Program.cs
@@ -0,0 +1,238 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Xml.Serialization;
+
+public static class Program
+{
+    static SortedList<int, Brick> bricks = new SortedList<int, Brick>();
+    public static HashSet<int> hasfallen = new HashSet<int>();
+
+    static SortedList<int, Brick> fallenbricks = new SortedList<int, Brick>();
+
+    public static int lowcount = 0, highcount = 0;
+    static int push;
+
+    static void Main()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        Console.WriteLine("Hello World on December 22th 2023!");
+        
+        string inputPath = @"..\..\..\AOC2023-22-Input.txt";
+        using (StreamReader inputFile = new StreamReader(inputPath))
+        {
+            string input;
+            // Read modules
+            while ((input = inputFile.ReadLine()) != null)
+            {
+                Match match = Regex.Match(input, @"^(?<x1>\d+),(?<y1>\d+),(?<z1>\d+)~(?<x2>\d+),(?<y2>\d+),(?<z2>\d+)$");
+                if (match.Success)
+                {
+                    Brick nb = new Brick(int.Parse(match.Groups["x1"].Value), int.Parse(match.Groups["y1"].Value), int.Parse(match.Groups["z1"].Value),
+                                         int.Parse(match.Groups["x2"].Value), int.Parse(match.Groups["y2"].Value), int.Parse(match.Groups["z2"].Value));
+                    bricks.Add(nb.SortKey(), nb);
+                }
+            }
+            inputFile.Close();
+            Console.WriteLine("Done reading bricks!");
+            Console.WriteLine("The bricks in sorted order from lowest and up are:");
+
+            // For all bricks
+            foreach (Brick b in bricks.Values)
+            {
+                Console.Write($"{b.x1},{b.y1},{b.z1} ~ {b.x2},{b.y2},{b.z2} => ");
+                b.Drop(bricks);
+                Console.WriteLine($"{b.x1},{b.y1},{b.z1} ~ {b.x2},{b.y2},{b.z2}");
+            }
+
+            Console.WriteLine("After dropping to rest the bricks are:");
+            int answer = 0, answer2 = 0, tmpchainreaction = 0;
+            foreach (Brick b in bricks.Values)
+            {
+                Console.Write($"Brick {b.x1},{b.y1},{b.z1} ~ {b.x2},{b.y2},{b.z2} => supports {b.supports.Count()} and is supported by {b.supportedby.Count()}. Disintegrateble? ");
+                if (b.Disintegrateable(bricks))
+                {
+                    answer++;
+                    Console.WriteLine("Yes!");
+                }
+                else
+                {
+                    Console.WriteLine("No!");
+
+                    hasfallen.Clear();
+                    fallenbricks.Clear();
+                    tmpchainreaction = b.ChainReaction2(bricks, fallenbricks);
+                    Console.WriteLine($"=> Will cause a chain reaction disintegrating {tmpchainreaction} bricks.");
+                    answer2 += tmpchainreaction;
+                }
+            }
+
+
+            Console.WriteLine($"Answer to part two is {answer2}!");
+            Console.WriteLine($"Hit any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
+
+class Brick
+{
+    public int sortkey;
+    public int x1;
+    public int y1;
+    public int z1;
+    public int x2;
+    public int y2;
+    public int z2;
+    public List<int> supports;
+    public List<int> supportedby;
+
+    public Brick(int x1, int y1, int z1, int x2, int y2, int z2)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.z1 = z1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.z2 = z2;
+        this.supports = new List<int>();
+        this.supportedby = new List<int>();
+    }
+
+    public Brick(Brick sb)
+    {
+        this.sortkey = sb.sortkey;
+        this.x1 = sb.x1;
+        this.y1 = sb.y1;
+        this.z1 = sb.z1;
+        this.x2 = sb.x2;
+        this.y2 = sb.y2;
+        this.z2 = sb.z2;
+        this.supports = new List<int>();
+        this.supportedby = new List<int>();
+    }
+
+    public int SortKey()
+    {
+        sortkey = Math.Min(z1, z2) * 100000 + Math.Min(y1, y2) * 100 + Math.Min(x1, x2);
+        return sortkey;
+    }
+
+    public int SortKeyTop()
+    {
+        return Math.Max(z1, z2) * 100000 + Math.Max(y1, y2) * 100 + Math.Max(x1, x2); ;
+    }
+
+    public void Drop(SortedList<int, Brick> bsl)
+    {
+        int minz = 1, tempminz = 1;
+        List<int> tempsupport = new List<int>();
+
+        foreach (Brick b in bsl.Values)
+        {
+            if (this.sortkey == b.sortkey)
+                break;
+
+            if (Math.Max(Math.Min(x1, x2), Math.Min(b.x1, b.x2)) <= Math.Min(Math.Max(x1, x2), Math.Max(b.x1, b.x2)) &&
+                Math.Max(Math.Min(y1, y2), Math.Min(b.y1, b.y2)) <= Math.Min(Math.Max(y1, y2), Math.Max(b.y1, b.y2)))
+            {
+                tempminz = Math.Max(b.z1, b.z2) + 1;
+                if (tempminz > minz)
+                {
+                    tempsupport.Clear();
+                }
+                if (tempminz >= minz)
+                {
+                    tempsupport.Add(b.sortkey);
+                    minz = tempminz;
+                }
+            }
+        }
+
+        int drop = Math.Min(z1, z2) - minz;
+        if (drop > 0)
+        {
+            z1 -= drop;
+            z2 -= drop;
+        }
+
+        foreach (int bsk in tempsupport)
+        {
+            this.supportedby.Add(bsk);
+            bsl[bsk].supports.Add(this.sortkey);
+        }
+    }
+
+    public bool Disintegrateable(SortedList<int, Brick> bsl)
+    {
+        foreach (int s in supports)
+        {
+            if (bsl[s].supportedby.Count() < 2) // The brick supports a brick with no other support
+                return false;
+        }
+        return true;
+    }
+
+    public int ChainReaction(SortedList<int, Brick> bsl, SortedList<int, Brick> fb)
+    {
+        fb.Add(this.SortKeyTop(), new Brick(this));
+        Program.hasfallen.Add(this.sortkey);
+
+        while (fb.Count() > 0)
+        {
+            int key = fb.First().Value.sortkey;
+            fb.Remove(fb.First().Key);
+
+            foreach (int s in bsl[key].supports)
+            {
+                int remainingsupport = 0;
+                foreach (int rs in bsl[s].supportedby)
+                    if (!Program.hasfallen.Contains(rs))
+                        remainingsupport++;
+
+                if (remainingsupport == 0) // The brick supports a brick with no other support
+                {
+                    if (Program.hasfallen.Add(bsl[s].sortkey)) // Add if not already fallen
+                        fb.Add(bsl[s].SortKeyTop(), new Brick(bsl[s]));
+                }
+            }
+        }
+        return Program.hasfallen.Count() - 1;
+    }
+
+    public int ChainReaction2(SortedList<int, Brick> bsl, SortedList<int, Brick> fb)
+    {
+        Program.hasfallen.Add(this.sortkey);
+
+        int fell = 0;
+        do
+        {
+            fell = 0;
+            foreach (Brick b in bsl.Values)
+            {
+                if (Program.hasfallen.Contains(b.sortkey)) // No need to check fallen bricks
+                    continue;
+
+                if (Math.Min(b.z1, b.z2) == 1) // On ground level
+                    continue;
+
+                int remainingsupport = 0;
+                foreach (int s in b.supportedby)
+                    if (!Program.hasfallen.Contains(s))
+                        remainingsupport++;
+
+                if (remainingsupport == 0) // The brick supports a brick with no other support
+                {
+                    Program.hasfallen.Add(b.sortkey); // Add if not already fallen
+                    fell++;
+                }
+            }
+            // Console.WriteLine($"{fell} bricks fell in this pass!");
+        } while (fell > 9);
+
+        return Program.hasfallen.Count() - 1;
+    }
+}
